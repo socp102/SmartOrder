f6a5507e3dee3962923b809cbd0e825b2bde2ec2
Eason <chcy1005@gmail.com>
2018-11-06 17:35:04 +0800

    Firestore 新增測試與範例

--- SmartOrder/MainViewController.swift
+++ SmartOrder/MainViewController.swift
@@ -15,6 +15,8 @@ class MainViewController: UIViewController {
     @IBOutlet weak var textFieldPassword: UITextField!
     
     var authHandle: AuthStateDidChangeListenerHandle?
+    var db: Firestore!
+    
     
     @IBAction func unwindSegue (_ segue: UIStoryboardSegue) {
         
@@ -25,7 +27,7 @@ class MainViewController: UIViewController {
         // Do any additional setup after loading the view.
         // Add the background gradient  背景調成漸層色
         view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
-        
+        db = Firestore.firestore()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -99,6 +101,95 @@ class MainViewController: UIViewController {
     
     @IBAction func signInUser(_ sender: Any) {
         signIn(email: "user@test.com", password: "123456")
+    }
+    
+    // Firestore 測試與範例 ============================================================================
+    
+    @IBAction func test(_ sender: Any) {
+        loadDocumentEqualTo()
+    }
+    
+    // 新增資料到 測試用集合 檔名自動生成
+    func addNewDocumentGeneratedID() {
+        var ref: DocumentReference? = nil
+        ref = db.collection("測試用集合").addDocument(data: [
+            "key": "值",
+            "name": "eason",
+            "born": 12345
+        ]) { error in
+            if let  error = error {
+                print("Error adding document: \(error)")
+            } else {
+                print("Document added with ID : \(ref!.documentID)")
+            }
+        }
+    }
+    
+    // 新增檔案 自訂檔名
+    func addNewDocument() {
+        db.collection("測試用集合").document("檔案名稱").setData([
+            "key": "value",
+            "說明": "用set 新增檔案，如果已經存在會覆蓋原本資料"
+        ]) { error in
+            if let  error = error {
+                print("Error writing document: \(error)")
+            } else {
+                print("Document successfully written!")
+            }
+        }
+        // 新增檔案 如果已經存在會新增
+        db.collection("測試用集合").document("檔案名稱").setData(["新增" : "新增的內容"], merge: true)
+    }
+    
+    // 更新檔案部分內容
+    func update() {
+        db.collection("測試用集合").document("檔案名稱").updateData([
+            "記錄最後更新時間": FieldValue.serverTimestamp()
+        ]) { error in
+            if let  error = error {
+                print("Error updating document: \(error)")
+            } else {
+                print("Document successfully updated!")
+            }
+        }
+    }
+    
+    // 讀取集合內所有檔案
+    func loadDatas() {
+        db.collection("測試用集合").getDocuments { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    print("\(document.documentID) ====> \(document.data())")
+                }
+            }
+        }
+    }
+    
+    // 讀取固定檔案
+    func loadDocument() {
+        db.collection("測試用集合").document("檔案名稱").getDocument { (document, err) in
+            if let document = document, document.exists {
+                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+                print("Document data: \(dataDescription)")
+            } else {
+                print("Document odes not exists")
+            }
+        }
+    }
+    
+    // 讀取符合條件的檔案
+    func loadDocumentEqualTo() {
+        db.collection("測試用集合").whereField("name", isEqualTo: "eason").getDocuments { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    print("\(document.documentID) ====> \(document.data())")
+                }
+            }
+        }
     }
     
 }
