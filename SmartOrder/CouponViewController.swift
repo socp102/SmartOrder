4300ed4bd43eca221b5f9a95f39e5a88759ce1d5
Boris <borischen0902@gmail.com>
2018-11-08 11:42:52 +0800

    push test

---
+++ SmartOrder/CouponViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CouponViewController.swift
+//  SmartOrder
+//
+//  Created by BorisChen on 2018/11/8.
+//  Copyright © 2018 Eason. All rights reserved.
+//
+
+import UIKit
+
+class CouponViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
