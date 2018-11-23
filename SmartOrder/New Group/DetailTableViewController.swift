cfba0824ad54ef7ea63887d012cdf803bc13d86b
imaginemanspeed <imaginemanspeed@yahoo.com.tw>
2018-11-23 10:07:42 +0800

    新增菜單內容

---
+++ SmartOrder/New Group/DetailTableViewController.swift
@@ -0,0 +1,222 @@
+//
+//  DetailTableViewController.swift
+//  SmartOrder
+//
+//  Created by Lu Kevin on 2018/11/19.
+//  Copyright © 2018年 Eason. All rights reserved.
+//
+
+import UIKit
+
+class DetailTableViewController: UITableViewController {
+
+    @IBOutlet weak var detailPrice: UILabel!
+    @IBOutlet weak var detailImage: UIImageView!
+    var menuSelectedNumber = 0
+    var detailImageNumber = 0
+    var currentPrice = ""
+    
+    
+    var hamburgerImages = ["牛肉漢堡","雞肉漢堡","豬肉漢堡"]
+    var hamburgerName = ["牛肉漢堡","雞肉漢堡","豬肉漢堡"]
+    var hamburgurPrice = ["70","80","90"]
+    
+    var spaghettiImages = ["紅醬義大利麵","青醬義大利麵","白醬義大利麵"]
+    var spaghettiName = ["紅醬義大利麵","青醬義大利麵","白醬義大利麵"]
+    var spaghettiPrice = ["70","80","90"]
+    
+    
+    var pizzaImages = ["起司披薩","番茄披薩","橄欖披薩"]
+    var pizzaName = ["起司披薩","番茄披薩","橄欖披薩"]
+    var pizzaPrice = ["70","80","90"]
+    
+    
+    var steakImages = ["牛菲力","牛肋排","炙燒牛排"]
+    var steakName = ["牛菲力","牛肋排","炙燒牛排"]
+    var steakPrice = ["70","80","90"]
+    
+    
+    var dessertImages = ["馬卡龍","巧克力蛋糕","聖代"]
+    var dessertName = ["馬卡龍","巧克力蛋糕","聖代"]
+    var dessertPrice = ["70","80","90"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        switch menuSelectedNumber {
+            
+        case 0:
+            
+            switch detailImageNumber{
+                
+            case 0:
+                detailImage.image = UIImage(named: hamburgerImages[detailImageNumber])
+                detailPrice.text? = hamburgurPrice[detailImageNumber]
+                currentPrice = hamburgurPrice[detailImageNumber]
+                resultItem = hamburgerName[detailImageNumber]
+            case 1:
+                detailImage.image = UIImage(named: hamburgerImages[detailImageNumber])
+                detailPrice.text? = hamburgurPrice[detailImageNumber]
+                currentPrice = hamburgurPrice[detailImageNumber]
+                resultItem = hamburgerName[detailImageNumber]
+
+            case 2:
+                detailImage.image = UIImage(named: hamburgerImages[detailImageNumber])
+                detailPrice.text? = hamburgurPrice[detailImageNumber]
+                currentPrice = hamburgurPrice[detailImageNumber]
+                resultItem = hamburgerName[detailImageNumber]
+
+            default:
+                break
+            }
+            
+            
+        case 1:
+            
+            switch detailImageNumber{
+                
+            case 0:
+                detailImage.image = UIImage(named: spaghettiImages[detailImageNumber])
+                detailPrice.text? = spaghettiPrice[detailImageNumber]
+                currentPrice = spaghettiPrice[detailImageNumber]
+                resultItem = spaghettiName[detailImageNumber]
+
+
+            case 1:
+                detailImage.image = UIImage(named: spaghettiImages[detailImageNumber])
+                detailPrice.text? = spaghettiPrice[detailImageNumber]
+                currentPrice = spaghettiPrice[detailImageNumber]
+                resultItem = spaghettiName[detailImageNumber]
+
+
+            case 2:
+                detailImage.image = UIImage(named: spaghettiImages[detailImageNumber])
+                detailPrice.text? = spaghettiPrice[detailImageNumber]
+                currentPrice = spaghettiPrice[detailImageNumber]
+                resultItem = spaghettiName[detailImageNumber]
+
+            default:
+                break
+            }
+        
+        case 2:
+            
+            switch detailImageNumber{
+                
+            case 0:
+                detailImage.image = UIImage(named: pizzaImages[detailImageNumber])
+                detailPrice.text? = pizzaPrice[detailImageNumber]
+                currentPrice = pizzaPrice[detailImageNumber]
+                resultItem = pizzaName[detailImageNumber]
+
+            case 1:
+                detailImage.image = UIImage(named: pizzaImages[detailImageNumber])
+                detailPrice.text? = pizzaPrice[detailImageNumber]
+                currentPrice = pizzaPrice[detailImageNumber]
+                resultItem = pizzaName[detailImageNumber]
+                
+            case 2:
+                detailImage.image = UIImage(named: pizzaImages[detailImageNumber])
+                detailPrice.text? = pizzaPrice[detailImageNumber]
+                currentPrice = pizzaPrice[detailImageNumber]
+                resultItem = pizzaName[detailImageNumber]
+
+            default:
+                break
+            }
+            
+        case 3:
+            
+            switch detailImageNumber{
+                
+            case 0:
+                detailImage.image = UIImage(named: steakImages[detailImageNumber])
+                detailPrice.text? = steakPrice[detailImageNumber]
+                currentPrice = steakPrice[detailImageNumber]
+                resultItem = steakName[detailImageNumber]
+
+
+            case 1:
+                detailImage.image = UIImage(named: steakImages[detailImageNumber])
+                detailPrice.text? = steakPrice[detailImageNumber]
+                currentPrice = steakPrice[detailImageNumber]
+                resultItem = steakName[detailImageNumber]
+
+
+            case 2:
+                detailImage.image = UIImage(named: steakImages[detailImageNumber])
+                detailPrice.text? = steakPrice[detailImageNumber]
+                currentPrice = steakPrice[detailImageNumber]
+                resultItem = steakName[detailImageNumber]
+                
+            default:
+                break
+            }
+            
+        case 4:
+            
+            switch detailImageNumber{
+                
+            case 0:
+                detailImage.image = UIImage(named: dessertImages[detailImageNumber])
+                detailPrice.text? = dessertPrice[detailImageNumber]
+                currentPrice = dessertPrice[detailImageNumber]
+                resultItem = dessertName[detailImageNumber]
+
+            case 1:
+                detailImage.image = UIImage(named: dessertImages[detailImageNumber])
+                detailPrice.text? = dessertPrice[detailImageNumber]
+                currentPrice = dessertPrice[detailImageNumber]
+                resultItem = dessertName[detailImageNumber]
+
+            case 2:
+                detailImage.image = UIImage(named: dessertImages[detailImageNumber])
+                detailPrice.text? = dessertPrice[detailImageNumber]
+                currentPrice = dessertPrice[detailImageNumber]
+                resultItem = dessertName[detailImageNumber]
+
+
+            default:
+                break
+            }
+        
+            
+        default:
+            break
+        }
+    }
+    
+    @IBOutlet weak var stepperCount: UILabel!
+    var resultPrice = ""
+    var resultCount = ""
+    var resultItem = ""
+    @IBAction func stepper(_ sender: UIStepper) {
+    
+        let count = Int(sender.value)
+        resultCount = String(count)
+        
+        stepperCount.text = String(count)
+        let price = Int(currentPrice)!
+        
+        resultPrice = String(price * count)
+        detailPrice?.text = resultPrice
+        
+    }
+    
+    @IBAction func addToOrder(_ sender: Any) {
+        
+    
+        
+    }
+    
+    
+   
+    
+    
+    
+    
+}
