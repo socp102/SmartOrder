584855567ad2bbdaea2aa5c03f5cae654404d06a
kimbely <kimbely0320@gmail.com>
2018-12-12 16:31:43 +0800

    0.6

--- SmartOrder/Member/InformationLists/modifyTableViewController.swift
+++ SmartOrder/Member/InformationLists/modifyTableViewController.swift
@@ -7,84 +7,74 @@
 //
 
 import UIKit
+import Firebase
 
 class modifyTableViewController: UITableViewController {
-
+    let fireBase = FirebaseCommunicator.shared
+    var objects = [Information]()
+    var information = Information()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        tableView.isScrollEnabled = false
+        
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    @IBAction func finalBtn(_ sender: UIButton) {
+        objects.append(information)
+        print("object: \(objects)")
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+    
+    @IBAction func emailtextField(_ sender: UITextField) {
+        guard let email = sender.text else {
+            return
+        }
+        information.email = email
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    @IBAction func nameTextField(_ sender: UITextField) {
+        guard let name = sender.text else {
+            return
+        }
+        information.name = name
+        
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    @IBAction func Gender(_ sender: UISwitch) {
+        if sender.isOn {
+            information.gender = "1"
+        } else {
+            information.gender = "2"
+        }
+        
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    @IBAction func datePicker(_ sender: UIDatePicker) {
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    @IBAction func phoneTextField(_ sender: UITextField) {
+        guard let phone = sender.text else {
+            return
+        }
+        information.phoneNum = phone
+        
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    
+    func modify() {
+        
+        guard let currentUserUid = Auth.auth().currentUser?.uid else {
+            print("uid fail")
+            return
+        }
+        /*
+        fireBase.updateData(collectionName: "account", documentName: currentUserUid, data: data) { (results, error) in
+            if let error = error {
+                print("error: \(error)")
+            }
+        }
+         */
+        
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
