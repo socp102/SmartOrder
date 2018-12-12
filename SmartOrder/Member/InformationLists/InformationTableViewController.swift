53f3bf38de1d32e5c17c15e05c479da162282fe4
kimbely <kimbely0320@gmail.com>
2018-12-12 16:34:18 +0800

    4,1

--- SmartOrder/Member/InformationLists/InformationTableViewController.swift
+++ SmartOrder/Member/InformationLists/InformationTableViewController.swift
@@ -7,14 +7,15 @@
 //
 
 import UIKit
+import Firebase
 
 
 class InformationTableViewController: UITableViewController {
     
-    
+    let fireBase = FirebaseCommunicator.shared
     var objects = [Information]()
-    var object = Information()
-    var type = LoadData()
+    var information = Information()
+    var loadinfo = ["":""]
     
     
     
@@ -24,81 +25,49 @@ class InformationTableViewController: UITableViewController {
     @IBOutlet weak var birthday: UILabel!
     @IBOutlet weak var phone: UILabel!
     
+    @IBOutlet weak var btn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.isScrollEnabled = false
+        loadinfonum()
         
-        load()
     }
     
         
-    func load() {
-        object = type.load() as! Information
-        email.text = object.email
-        name.text = object.name
-        birthday.text = object.birthday
-        phone.text = object.phoneNum
+    func loadinfonum() {
+        //下載
+        guard let currentUserUid = Auth.auth().currentUser?.uid else {
+            print("uid fail")
+            return
+        }
+        fireBase.loadData(collectionName: "account", documentName: currentUserUid) { (result, error) in
+            if let error = error {
+                print("error: \(error)")
+            }else{
+                let result = result as! [String:Any]
+                self.loadinfo = result as! [String : String]
+                print("loadinfo : \(self.loadinfo)")
+                self.information.birthday = self.loadinfo["Birthday"] ?? ""
+                self.information.email = self.loadinfo["email"]!
+                self.information.phoneNum = self.loadinfo["PhoneNumber"] ?? ""
+                self.information.gender = self.loadinfo["Gender"] ?? ""
+                self.information.name = self.loadinfo["name"] ?? ""
+                self.email.text = self.information.email
+                self.name.text = self.information.name
+                self.birthday.text = self.information.birthday
+                self.phone.text = self.information.phoneNum
+                self.objects.append(self.information)
+                
+                
+                
+            }
+        }
     }
-        
     
-
-
-    // MARK: - Table view data source
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        //
     }
-    */
 
 }
